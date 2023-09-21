967af163cca795e2d63b570a324d2d244ca52463
SurajKumbhar1994 <kumbharsuraj154@gmail.com>
2023-09-21 23:30:58 +0530

    Assignment No1 done
    Assignment No1 done

---
+++ 01_variable.js
@@ -0,0 +1,26 @@
+var myFullName;
+console.log(myFullName);
+
+var myFullName = "Suraj Kumbhar";
+console.log(myFullName);
+
+myFullName = "Suraj Shashikant Kumbhar";
+console.log(myFullName);
+
+var myLove = "I love only JS";
+console.log(myLove);
+
+var firstName = "Suraj";
+console.log("First Name:", firstName);
+var lastName = "Kumbhar";
+console.log("Last Name:", lastName);
+var age = "28";
+console.log("Age:", age);
+var address = "Kolhapur";
+console.log("Address:", address);
+var pinCode = "416 118";
+console.log("Pin Code:", pinCode);
+var gender = "Male";
+console.log("Gender:", gender);
+var isMarried = "No";
+console.log("Married Status:", isMarried);
